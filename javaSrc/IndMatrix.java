74d7606b4fbeff1a14eafc861ed76fbe4e6dee79
rmit-s3601500-yifan-zhao <s3601500@student.rmit.edu.au>
2018-04-11 19:08:04 +1000

    progressing

--- javaSrc/IndMatrix.java
+++ javaSrc/IndMatrix.java
@@ -12,32 +12,122 @@ import java.util.*;
 public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 {
 
+	protected final ArrayList<ArrayList<Integer>> indMatrixGraph;
+	protected final ArrayList<T> indMatrixVertex;
+	protected final ArrayList<ArrayList<T>> indMatrixEdge;
+	
 	/**
 	 * Contructs empty graph.
 	 */
     public IndMatrix() {
-    	// Implement me!
+    	
+		indMatrixGraph = new ArrayList<ArrayList<Integer>>();
+		indMatrixVertex = new ArrayList<T>();
+		indMatrixEdge = new ArrayList<ArrayList<T>>();
+		// Implement me!
     } // end of IndMatrix()
     
     
     public void addVertex(T vertLabel) {
-        // Implement me!
+        
+		ArrayList<Integer> tempArrayList = new ArrayList<Integer>();
+
+		if(indMatrixEdge.size() > 0){
+			for(int i = 0; i < indMatrixEdge.size(); i++){
+				tempArrayList.add(0);
+			}
+			indMatrixGraph.add(tempArrayList);
+		}	
+		
+		indMatrixVertex.add(vertLabel);
+		indMatrixGraph.add(new ArrayList<Integer>());
+		
+
+/*		for(int i = 0; i < indMatrixGraph.size();i++){
+			for(int j = 0; j < indMatrixGraph.get(i).size();j++){
+				System.out.print(indMatrixGraph.get(i).get(j));
+			}
+			System.out.print("\n");
+		}*/
+		// Implement me!
     } // end of addVertex()
 	
     
     public void addEdge(T srcLabel, T tarLabel) {
-        // Implement me!
+        int src = 0;
+		int tar = 0;
+		ArrayList<T> tempList = new ArrayList<T>();
+		tempList.add(srcLabel);
+		tempList.add(tarLabel);
+		indMatrixEdge.add(tempList);
+		
+		for(int i = 0; i < indMatrixGraph.size(); i++){
+			if(indMatrixVertex.get(i).equals(srcLabel)){
+				src = i;
+				
+			}
+			if(indMatrixVertex.get(i).equals(tarLabel)){
+				tar = i;
+			}
+		}
+		
+		for(int i = 0; i < indMatrixGraph.size(); i++){
+			if(i == src || i == tar){
+				indMatrixGraph.get(i).add(1);
+			}
+			else{
+				indMatrixGraph.get(i).add(0);
+			}
+		}
+		
+		
+		
+		System.out.printf("s=%d t=%d\n",src,tar);
+		
+/*		for(int i=0; i<indMatrixGraph.size();i++){
+			for(int j=0; j<indMatrixGraph.get(i).size();j++){
+				System.out.print(indMatrixGraph.get(i).get(j));
+			}
+			System.out.print("\n");
+		}
+		
+		for(int i = 0; i < indMatrixEdge.size(); i++){
+			System.out.print(indMatrixEdge.get(i));
+			System.out.printf("%d", i);
+		}*/
+		
+		// Implement me!
     } // end of addEdge()
 	
 
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
-        
+
+		for (int i = 0; i < indMatrixGraph.size(); i++){
+			if (indMatrixVertex.get(i).equals(vertLabel)){
+				for (int j = 0; j < indMatrixGraph.get(i).size(); j++){
+					if(indMatrixGraph.get(i).get(j) == 1){
+						if(indMatrixVertex.get(i).equals(indMatrixEdge.get(j).get(0))){
+							
+							neighbours.add(indMatrixEdge.get(j).get(1));
+						}
+						else{
+							neighbours.add(indMatrixEdge.get(j).get(0));
+						}
+					}
+				}
+				
+			}
+		}
+		
+		for (int i = 0; i < neighbours.size(); i++){
+			System.out.print(neighbours.get(i));
+			System.out.printf("%d", i);
+		}
         // Implement me!
         
         return neighbours;
     } // end of neighbours()
-    
     
     public void removeVertex(T vertLabel) {
         // Implement me!
@@ -67,3 +157,5 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
     } // end of shortestPathDistance()
     
 } // end of class IndMatrix
+
+
